bb42c04bbb645e84967b36a1241edac5a0786736
Vimlesh Kumar Kanaujiya <kanaujiyavimleshkumar@gmail.com>
2023-05-04 14:39:23 +0530

    removed src/db.js from gitignore

---
+++ src/db.js
@@ -0,0 +1,10 @@
+const github = {
+    baseURL : "https://api.github.com/graphql",
+    username : "vimleshkumarkanaujiya",
+    headers : {
+        "Content-Type" : "application/json",
+        Authorization : "bearer key"
+    }
+    };
+
+    export default github;
